95e17d7527fb8fccb0b03c6561be44fdd76bd983
Marcus Lindblom <m@marcuslindblom.com>
2020-12-03 12:21:17 +0100

    Fixes document format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 declare -A grades=(
-   ['A+']=7
-   ['A']=6
-   ['B']=5
-	 ['C']=4
-	 ['D']=3
-	 ['E']=2
-	 ['F']=1
+	['A+']=7
+	['A']=6
+	['B']=5
+	['C']=4
+	['D']=3
+	['E']=2
+	['F']=1
 )
 
 GRADE=$2
 
-RATING=`curl -s -L "https://securityheaders.io/?hide=on&q=$1" -I | sed -En 's/x-grade: (.*)/\1/p' | tr -d '\r'`
+RATING=$(curl -s -L "https://securityheaders.io/?hide=on&q=$1" -I | sed -En 's/x-grade: (.*)/\1/p' | tr -d '\r')
 
 echo "::set-output name=rating::$RATING"
 
