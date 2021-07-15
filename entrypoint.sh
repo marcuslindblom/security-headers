c973d753921a4bdd60c2c10c166ebdd66ba8c6b3
Matt Thornton <matthew.thornton13@gmail.com>
2021-07-15 08:49:15 +0100

    Add followRedirects parameter.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,13 @@ declare -A grades=(
 	['F']=1
 )
 
-GRADE=$2
+if [ "$2" = 'true' ]; then
+    FOLLOW_REDIRECTS='on'
+fi
 
-RATING=$(curl -s -L "https://securityheaders.com/?hide=on&q=$1" -I | sed -En 's/x-grade: (.*)/\1/p' | tr -d '\r')
+GRADE=$3
+
+RATING=$(curl -s -L "https://securityheaders.com/?hide=on&followRedirects=$FOLLOW_REDIRECTS&q=$1" -I | sed -En 's/x-grade: (.*)/\1/p' | tr -d '\r')
 
 echo "::set-output name=rating::$RATING"
 
