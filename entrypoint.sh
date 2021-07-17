daa23914e2c1560e45da408474059e45d6d0c922
Matt Thornton <matthew.thornton13@gmail.com>
2021-07-17 11:00:17 +0100

    Default followRedirects to 'on'.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,17 @@ if [ $# == 0 ]; then
     echo "Usage: url [grade] [followRedirects]"
     echo "* url: URL to analyse."
     echo "* grade: The desired security grade of your HTTP response headers. Possible grades: A+, A, B, C, D, E, F. Defaults to B."
-    echo "* followRedirects: Follow redirects. Defaults to false, set to true to enable."
+    echo "* followRedirects: Follow redirects. Defaults to on, set to off or false to disable."
     exit 1
 fi
 
+
 GRADE=${2:-'B'}
-FOLLOW_REDIRECTS=${3:-''}
+FOLLOW_REDIRECTS=${3-'1'}
+FOLLOW_REDIRECTS=${FOLLOW_REDIRECTS/true/1}
+FOLLOW_REDIRECTS=${FOLLOW_REDIRECTS/on/1}
+FOLLOW_REDIRECTS=${FOLLOW_REDIRECTS/false/0}
+FOLLOW_REDIRECTS=${FOLLOW_REDIRECTS/off/0}
 
 declare -A grades=(
 	['A+']=7
@@ -20,10 +25,6 @@ declare -A grades=(
 	['E']=2
 	['F']=1
 )
-
-if [ "$FOLLOW_REDIRECTS" = 'true' ]; then
-    FOLLOW_REDIRECTS='on'
-fi
 
 RATING=$(curl -s -L "https://securityheaders.com/?hide=on&followRedirects=$FOLLOW_REDIRECTS&q=$1" -I | sed -En 's/x-grade: (.*)/\1/p' | tr -d '\r')
 
